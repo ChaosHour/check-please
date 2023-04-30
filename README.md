bcf892736da502866c4d3d41eda0713fa8686841
ChaosHour <kurt_lv@cox.net>
2023-04-30 14:48:03 -0700

    Adding files

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # check-please
 A shell script that runs chkrootkit, rkhunter, and ClamAV with some others depending on the OS.
+
+# Usage:
+./checkme.sh or ./checkme.sh -h for homedir or ./checkme.sh -s for full system scan
+
+
+# Dependencies for MacOS:
+- `chkrootkit`
+- `rkhunter`
+- `ClamAV`
+- `Lynis`
+
+# Dependencies for Debian-based distributions:
+- `chkrootkit`
+- `rkhunter`
+- `ClamAV`
+- `Unhide`
+- `debsums`
+- `Lynis`
+
+# Example Usage and output:
+```bash
+% ./checkme.sh
+ Running chkrootkit [+]
+Password:
+not infected
+not infected
+not infected
+not infected
+not infected
+not infected
+ Running rkhunter  [+]
+[ Rootkit Hunter version 1.4.6 ]
+
+Checking rkhunter data files...
+  Checking file mirrors.dat                                  [ No update ]
+  Checking file programs_bad.dat                             [ No update ]
+  Checking file backdoorports.dat                            [ No update ]
+  Checking file suspscan.dat                                 [ No update ]
+  Checking file i18n/cn                                      [ No update ]
+  Checking file i18n/de                                      [ No update ]
+  Checking file i18n/en                                      [ No update ]
+  Checking file i18n/tr                                      [ No update ]
+  Checking file i18n/tr.utf8                                 [ No update ]
+  Checking file i18n/zh                                      [ No update ]
+  Checking file i18n/zh.utf8                                 [ No update ]
+  Checking file i18n/ja                                      [ No update ]
+[ Rootkit Hunter version 1.4.6 ]
+File updated: searched for 169 files, found 97
+Warning: The command '/usr/bin/fuser' has been replaced by a script: /usr/bin/fuser: Perl script text executable
+Warning: The command '/usr/bin/whatis' has been replaced by a script: /usr/bin/whatis: POSIX shell script text executable, ASCII text
+Warning: The command '/usr/bin/shasum' has been replaced by a script: /usr/bin/shasum: Perl script text executable
+Warning: Checking for possible rootkit strings    [ Warning ]
+Warning: No system startup files found.
+Warning: The SSH configuration option 'PermitRootLogin' has not been set.
+         The default value may be 'yes', to allow root access.
+Warning: The SSH configuration option 'Protocol' has not been set.
+         The default value may be '2,1', to allow the use of protocol version 1.
+Warning: Hidden file found: /usr/share/man/man5/.rhosts.5: troff or preprocessor input text, ASCII text
+ Running a lynis audit scan  [+]
+[+] Software: Malware
+  - Checking chkrootkit                                       [ FOUND ]
+  - Checking Rootkit Hunter                                   [ FOUND ]
+  - Checking ClamAV scanner                                   [ FOUND ]
+  - Malware software components                               [ FOUND ]
+    - Rootkit scanner                                         [ FOUND ]
+    - Installed malware scanner                               [ FOUND ]
+  - Malware scanner        [V]
+ Usage: ./checkme.sh -h for homedir or ./checkme.sh -s for full system scan
+```
---
+++ checkme.sh
@@ -0,0 +1,164 @@
+#! /usr/bin/env zsh
+#
+#set -xv
+#set -o errexit
+#set -o nounset
+set -o pipefail
+###############################################################################################
+# Purpose: This script is used to check for rootkits and other malware plus viruses.
+# Created by: Kurt Larsen
+# Date: 2023-04-26
+# Version: 1.0
+#
+# Usage: ./checkme.sh or ./checkme.sh -h for homedir or ./checkme.sh -s for full system scan
+#
+###############################################################################################
+
+
+
+# Set constants - Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+
+# You must use sudo to run this script
+#if [ $EUID -ne 0 ]; then
+#    echo -e "$COL_YELLOW Use sudo to run this script $COL_RED [-] $COL_RESET"
+#    exit 1
+#fi
+
+# Variables
+
+# get the homedir path for the sudo user running the script
+#homedir=$(eval echo ~$SUDO_USER)
+
+# get the homedir path for the user running the script
+homedir=$(eval echo ~$USER)
+
+# test if tmp directory exists
+if [ ! -d "${homedir}/tmp" ]; then
+    # create tmp directory if it does not exist and let the user know
+    echo -e "$COL_YELLOW Creating tmp directory -> ${homedir}/tmp $COL_GREEN [+] $COL_RESET"
+       mkdir "${homedir}/tmp"
+fi
+
+# add a variable SUSPSCAN_TEMP to export
+export SUSPSCAN_TEMP=${homedir}/tmp
+
+
+# Functions
+# Check for rootkits
+fCheck()  {
+    if sudo find /lib* -type f -name libkeyutils.so* -exec ls -lrtha {} \;
+    then
+        echo -e "$COL_YELLOW Result filesize should be less then 15KB $COL_RED [-] $COL_RESET"
+    else
+        echo -e "$COL_YELLOW Result filesize is less then 15KB $COL_GREEN  [+] $COL_RESET"
+    fi
+
+    if sudo find /lib* -type f -name libproc.so* -exec ls -lrtha {} \;
+    then
+        echo -e "$COL_YELLOW Result filesize should be less then 15KB $COL_RED [-] $COL_RESET"
+    else
+        echo -e "$COL_YELLOW Result filesize is less then 15KB $COL_GREEN [+] $COL_RESET"
+    fi
+    
+}
+# Check for rootkits
+fCheck2() {
+    if sudo find /lib* -type f -name libns2.so
+    then
+        echo -e "$COL_YELLOW Result should return null $COL_RED [-] $COL_RESET"
+    else
+        echo -e "$COL_YELLOW Result should return null $COL_GREEN [+] $COL_RESET"
+    fi
+
+    if sudo netstat -nap | grep "@/proc/udevd"
+    then
+        echo -e "$COL_YELLOW Result should return null $COL_RED [-] $COL_RESET"
+    else
+        echo -e "$COL_YELLOW Result should return null $COL_GREEN [+] $COL_RESET"
+    fi
+}
+
+# Check for compromised sshd
+sCheck() {
+echo -e "$COL_YELLOW Checking for compromised sshd $COL_GREEN [+] $COL_RESET"
+   ssh -H 2>&1 | grep -e illegal -e unknown > /dev/null && echo "System clean" || echo "System infected"
+
+echo -e "$COL_YELLOW Checking hash on /usr/sbin/tcpd $COL_GREEN [+] $COL_RESET"
+   sudo find / -name tcpd -ls 2>&1 | grep -iv 'Permission denied'
+
+   sudo echo "cd9cfc19df7f0e4b7f9adfa4fe8c5d74caa53d86 /usr/sbin/tcpd" | shasum -a 1 -
+
+}
+
+# Check for rootkits
+rKits() {
+echo -e "$COL_YELLOW Running chkrootkit$COL_GREEN [+] $COL_RESET"
+   sudo chkrootkit -x 2>&1 | grep -Ei 'infected|not infected' | grep -v grep
+
+echo -e "$COL_YELLOW Running rkhunter $COL_GREEN [+] $COL_RESET"
+   sudo su - root -c '/usr/local/bin/rkhunter --update'
+   sudo su - root -c '/usr/local/bin/rkhunter --propupd'
+   sudo su - root -c '/usr/local/bin/rkhunter -c --enable all --disable none --rwo'
+}
+# Check for rootkits
+dChecks() {
+echo -e "$COL_YELLOW Running unhide $COL_GREEN [+] $COL_RESET"
+   sudo unhide -f sys -r
+   sudo unhide-tcp
+   sudo unhide brute
+
+echo -e "$COL_YELLOW Verifying installed packages using debsums $COL_GREEN [+] $COL_RESET"
+   sudo dpkg-query -S $(sudo debsums -c 2>&1 | sed -e "s/.*file \(.*\) (.*/\1/g") | cut -d: -f1 | sort -u
+}
+
+# Run Lynis scan
+lCheck() {
+echo -e "$COL_YELLOW Running a lynis audit scan $COL_GREEN [+] $COL_RESET"
+   lynis audit system --quick | grep -iE 'malware|clamav|rootkit|chkrootkit|rkhunter|unhide|debsums'
+}
+
+
+
+# Check for OS type
+if [[ "$OSTYPE" == darwin* ]]; then
+  # code for MacOs
+  #fCheck
+  rKits
+  lCheck
+else
+  if [[ "$OSTYPE" == linux-gnu ]]; then
+    # code for Debian
+    fCheck
+    fCheck2
+    sCheck
+    rKits
+    dChecks
+    lCheck     
+  fi
+fi
+
+
+# Create a case switch if you want to scan the entire system or just the homedir
+case $1 in
+    -h) echo -e "$COL_YELLOW updating clamav virus defs $COL_GREEN [+] $COL_RESET"
+           freshclam -v
+        echo -e "$COL_YELLOW Running clamscan on homedir $COL_GREEN [+] $COL_RESET"
+           clamscan -i -r --recursive=yes --scan-mail=yes --scan-pdf=yes --scan-html=yes --scan-archive=yes --phishing-scan-urls=yes --exclude-dir=infected --move="${homedir}/infected" ${homedir}
+        ;;
+    -s) echo -e "$COL_YELLOW Running full system scan with clamav $COL_GREEN [+] $COL_RESET"
+           clamscan -i -r --recursive=yes --scan-mail=yes --scan-pdf=yes --scan-html=yes --scan-archive=yes --phishing-scan-urls=yes --exclude-dir=infected --move="${homedir}/infected" /
+        ;;
+    *) echo -e "$COL_YELLOW Usage: $0 -h for homedir or $0 -s for full system scan $COL_RESET"
+        ;;
+esac
+
+exit 1
